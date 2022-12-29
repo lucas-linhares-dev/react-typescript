4db169f12a74710970352b19a79e80d6faf69733
Lucas <lucas_linhares_@hotmail.com>
2022-12-28 21:56:32 -0300

    Resvisando listas

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,31 @@
-import { useRef } from 'react';
+import { useRef, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { useUsuarioLogado } from '../../shared/hooks';
 
 
 export const Dashboard = () => {
 
+    const [lista, setLista] = useState<string[]>([])
+
     const { nomeUsuario } = useUsuarioLogado()
 
     const counterRef = useRef(0)
+
+    const addLista: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if(e.key === 'Enter'){
+            if(e.currentTarget.value.length === 0) return
+
+            const newValue = e.currentTarget.value
+
+            e.currentTarget.value = ''
+
+            setLista((oldLista) => {
+                return [...oldLista, newValue]
+            })
+        }
+
+    },[])
+
     
     return(
         <div>
@@ -17,6 +35,13 @@ export const Dashboard = () => {
             <button onClick={(e) => counterRef.current++}>Somar</button>
             <button onClick={() => console.log(counterRef.current)}>Mostrar</button>
             <Link to={"/entrar"}>Entrar</Link>
+            <hr></hr>
+            <input type="text" onKeyDown={addLista} />
+            <ul>
+                {lista.map((value, index) => {
+                    return <li key={value}>{value}</li>
+                })}
+            </ul>
         </div>
 
     )
