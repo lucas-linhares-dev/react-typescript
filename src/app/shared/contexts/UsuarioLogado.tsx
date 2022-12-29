4b4c77f02df0f4d8feecb80f69a357300d14fb19
Lucas <lucas_linhares_@hotmail.com>
2022-12-28 21:18:18 -0300

    Passando states no contexto

--- src/app/shared/contexts/UsuarioLogado.tsx
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import { useCallback } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 
 
 interface IUsuarioLogadoContextData {
@@ -16,12 +16,20 @@ interface IUsuarioLogadoProviderProps {
 
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProviderProps> = ({children}) => {
 
+    const [nome, setNome] = useState('')
+
+    useEffect(() => {
+        setTimeout(() => {
+            setNome('Lucas')
+        }, 1000)
+    })
+
     const handleLogout = useCallback(() => {
         console.log('Usuário deslogado.')
     }, [])
 
     return(
-        <UsuarioLogadoContext.Provider value={{nomeUsuario: 'Pedro', logout: handleLogout }}>
+        <UsuarioLogadoContext.Provider value={{nomeUsuario: nome, logout: handleLogout }}>
             {children}
         </UsuarioLogadoContext.Provider>
     )
